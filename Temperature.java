1a63f700ccadb57f50bae14d710e1266516f44ee
santosr795 <36488171+santosr795@users.noreply.github.com>
2018-09-14 21:50:25 -0400

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,64 @@
+import java.util.Scanner; 
+public class Temperature {
+	Scanner keyboard = new Scanner(System.in);
+
+	public void read() {
+		int inputTemperature = keyboard.nextInt(); 
+		
+	
+	}//End read 
+	private Temperature set() {
+		Temperature answer = new Temperature(); 
+		
+		return answer; 
+	}//End Set 
+	public Temperature add(int value) {
+		Temperature answer = new Temperature(); 
+		
+		return answer;
+	}//End Add
+	public Temperature subtract(int value) {
+		Temperature answer = new Temperature();
+		
+		return answer; 
+	}//End Subtract
+	public Temperature multiply(int value) {
+		Temperature answer= new Temperature(); 
+		
+		return answer; 
+	}//End Multiply 
+	public Temperature divide(int value) {
+		Temperature answer = new Temperature(); 
+		
+		return answer; 
+	}//End Divide 
+	public boolean equals(Temperature temperature) {
+		boolean answer = true; 
+		return answer; 
+	}//End equals
+	public boolean greaterThan(Temperature temperature) {
+		boolean answer = true; 
+		return answer; 
+	}//End greaterThan
+	public int toCalvin() {
+		int answer = 0;
+		
+		return answer; 
+	}//End toCalvin 
+
+	public int toKelvin() {
+		int answer = 0; 
+		
+		return answer; 
+	}//End ToKelvin
+	public int toFahrenheit() {
+		int answer = 0 ; 
+		
+		return answer; 
+	}//ToFahrenheit
+	public String toString() {
+		
+		return ""; 
+	}//End toString 
+}//End of Class
+
