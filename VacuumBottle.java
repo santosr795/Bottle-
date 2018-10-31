f28f8a28d0e6933cd6892321e88363c2687b0bd8
Ronald Santos <36488171+santosr795@users.noreply.github.com>
2018-10-30 23:26:46 -0400

    Update VacuumBottle.java

--- VacuumBottle.java
+++ VacuumBottle.java
@@ -1,11 +1,14 @@
 
+import java.util.Arrays;
+
 public class VacuumBottle {
 	private Temperature temp = new Temperature(); 
 	private Bottle bottle = new Bottle(); 
+	 Bottle  [] firstArray = new Bottle [(int) Math.random()*5+1]; 
+		Bottle [] secondArray = new Bottle [(int) Math.random() * 5 +1];
+		Bottle [] thirdArray = new Bottle [(int) Math.random()*5+1];
 	public static void main(String [] args) {
-		int [] firstArray = new int [(int) Math.random()*5+1]; 
-		int [] secondArray = new int [(int) Math.random() * 5 +1];
-		int [] thirdArray = new int [(int) Math.random()*5+1];
+		
 	}
 	VacuumBottle(){
 		this.temp = new Temperature( 0, ""); 
@@ -13,15 +16,19 @@ public class VacuumBottle {
 		
 	}
 		 
-	VacuumBottle(int value){
-		
-		bottle = new Bottle(value); 
-	}
 	
+	VacuumBottle(Bottle bottle2) {
+		Bottle pennies = null;
+		pennies.set(bottle2);
+		}
+	VacuumBottle(int value ){
+		Bottle pennies= null; 
+		pennies.set(value);		
+	}
 	public VacuumBottle add(VacuumBottle value) {
 		VacuumBottle answer; 
 		double tempo; 
-		answer = this.temp + value.temp;  
+		  
 		return value; 
 	}
 	public VacuumBottle subtract(VacuumBottle value) {
@@ -30,7 +37,7 @@ public class VacuumBottle {
 	public VacuumBottle divide(int value) {
 		VacuumBottle answer; 
 		
-		return answer = new VacuumBottle(value);
+		return answer;
 	}
 	public boolean equals(VacuumBottle value) {
 		return true;
@@ -48,10 +55,18 @@ public class VacuumBottle {
 			array[x] = t1;
 		}
 	}
-	public static VacuumBottle average(int [] array) {
-		VacuumBottle answer; 
+	public static double average(Bottle [] array) {
+		double answer; 
+		double sum = 0 ; 
 		int i= 0;
-		return answer = new VacuumBottle(i) ;
+		for(int x = 0; x < array.length;x++) {
+			Bottle passArray = array[x]; 
+			Double value =  new Double(passArray.toString());
+			sum = sum + value; 
+		} 
+		double average; 
+		 
+		return average = sum/array.length; 
 	}
 	public static void printVacuumBottle(Bottle [] array) {
 		for(int x = 0; x < array.length; x++ ) {
@@ -60,13 +75,37 @@ public class VacuumBottle {
 		}
 		 
 	}
-	public static VacuumBottle copyLargestArray(VacuumBottle []arrayOne, VacuumBottle [] arrayTwo, VacuumBottle [] arrayThree) {
-		return arrayOne;
+	public static Bottle[] copyLargestArray(Bottle[] arrayOne, Bottle [] arrayTwo, Bottle [] arrayThree) {
+		Bottle [] newArray ; 
+		if (arrayOne.length > arrayTwo.length && arrayOne.length > arrayThree.length) {
 	
+			newArray = arrayOne.clone();
+		}
+		
+		else if (arrayTwo.length > arrayOne.length && arrayTwo.length > arrayThree.length) 
+		{
+			newArray = arrayTwo.clone(); 
+		}
+		else if (arrayThree.length > arrayOne.length && arrayThree.length > arrayTwo.length)
+		{
+			newArray = arrayTwo.clone(); 
+		}
+			return newArray; 
 	}
 	
-	public static VacuumBottle largestArray(VacuumBottle arrayOne, VacuumBottle [] arrayTwo, VacuumBottle [] arrayThree) {
-	
+	public static Bottle[] largestArray(Bottle []arrayOne, Bottle [] arrayTwo, Bottle [] arrayThree) {
+		if (arrayOne.length > arrayTwo.length && arrayOne.length > arrayThree.length) {
+		
+		}
+		
+		else if (arrayTwo.length > arrayOne.length && arrayTwo.length > arrayThree.length) 
+		{
+			
+		}
+		else if (arrayThree.length > arrayOne.length && arrayThree.length > arrayTwo.length)
+		{
+			
+		}
 		return arrayOne; 
 	
 	}
